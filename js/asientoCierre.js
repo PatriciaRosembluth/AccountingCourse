5362d78e76309b7b2940cf6bb05e62c08e2f1003
PattyRosembluth <patty_rosembluth@hotmail.com>
2016-01-11 01:25:23 -0400

    Proceso contable completo

--- js/asientoCierre.js
+++ js/asientoCierre.js
@@ -12,7 +12,6 @@ function Add(){
         "<td><input type='text'/></td>"+
         "<td><input type='text'/></td>"+
         "<td><input type='text'/></td>"+
-        "<td><input type='text'/></td>"+
         "<td><img src='/AccountingCourse/img/disk.jpg' class='btnSave'><img src='/AccountingCourse/img/delete.png' class='btnDelete'/></td>"+
         "</tr>");
     
@@ -26,14 +25,12 @@ function Save(){
     var tdDetail = par.children("td:nth-child(2)");
     var tdDebe = par.children("td:nth-child(3)");
     var tdHaber = par.children("td:nth-child(4)");
-    var tdSaldo = par.children("td:nth-child(5)");
-    var tdButtons = par.children("td:nth-child(6)");
+    var tdButtons = par.children("td:nth-child(5)");
 
     tdDate.html(tdDate.children("input[type=text]").val());
     tdDetail.html(tdDetail.children("input[type=text]").val());
     tdDebe.html(tdDebe.children("input[type=text]").val());
     tdHaber.html(tdHaber.children("input[type=text]").val());
-    tdSaldo.html(tdSaldo.children("input[type=text]").val());
     tdButtons.html("<img src='/AccountingCourse/img/delete.png' class='btnDelete'/><img src='/AccountingCourse/img/pencil.png' class='btnEdit'/>");
 
     $(".btnEdit").bind("click", Edit);
@@ -46,14 +43,12 @@ function Edit(){
     var tdDetail = par.children("td:nth-child(2)");
     var tdDebe = par.children("td:nth-child(3)");
     var tdHaber = par.children("td:nth-child(4)");
-    var tdSaldo = par.children("td:nth-child(5)");
-    var tdButtons = par.children("td:nth-child(6)");
+    var tdButtons = par.children("td:nth-child(5)");
 
     tdDate.html("<input type='text' id='txtDate' value='"+tdDate.html()+"'/>");
     tdDetail.html("<input type='text' id='txtDetail' value='"+tdDetail.html()+"'/>");
     tdDebe.html("<input type='text' id='txtDebe' value='"+tdDebe.html()+"'/>");
     tdHaber.html("<input type='text' id='txtHaber' value='"+tdHaber.html()+"'/>");
-    tdSaldo.html("<input type='text' id='txtSaldo' value='"+tdSaldo.html()+"'/>");
     tdButtons.html("<img src='/AccountingCourse/img/disk.jpg' class='btnSave'/>");
 
     $(".btnSave").bind("click", Save);
