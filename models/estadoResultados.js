8c6ffa1a56b7028d6d5ec9c7a7aca8275168f3ab
Patrica Rosembluth <patty_rosembluth@hotmail.com>
2016-04-13 19:43:42 -0400

    gastos de operaciones en estado de resultados

--- models/estadoResultados.js
+++ models/estadoResultados.js
@@ -6,19 +6,24 @@ function cargarDatos() {
 }
 
 function generateEstadoResultados(){
+	gastos = 0;
+	stringGastos="";
 	for (var i = 0; i < estadoResultados.length; i=i+2) {
 		if (estadoResultados[i]==="ventas") {
 			document.getElementById("ventas").innerHTML = estadoResultados[i]+" --------------------------- " + estadoResultados[i+1];
-			ventas = estadoResultados[i+1];  
-		}
-		if (estadoResultados[i]==="costo de ventas") {
+			ventas = parseInt(estadoResultados[i+1]);  
+		}else if(estadoResultados[i]==="costo de ventas") { 
 			document.getElementById("costo-ventas").innerHTML = estadoResultados[i]+" ------------ " + estadoResultados[i+1];
-			costoVentas = estadoResultados[i+1];
-		}
-		if (estadoResultados[i]==="utilidad neta") {
-			document.getElementById("utilidad-neta").innerHTML = estadoResultados[i]+" ---------------------------------- " + estadoResultados[i+1];
+			costoVentas = parseInt(estadoResultados[i+1]);
+		}else if (estadoResultados[i]==="utilidad neta") { 
+			document.getElementById("utilidad-neta").innerHTML = estadoResultados[i].toUpperCase()+" ----------------------------------------------------- " + estadoResultados[i+1];
+		}else if (estadoResultados[i]!= "almacenes") {
+			gastos+=parseInt(estadoResultados[i+1]);
+			stringGastos+="<i><h3>"+estadoResultados[i]+" ---------------- " + estadoResultados[i+1]+"</h3></i>"
 		}
 	}
+	$('#gastos').empty().append(stringGastos);
 	utilidadBruta = ventas - costoVentas;
+	document.getElementById("total-gastos").innerHTML = "total gastos --------------------------------- " + gastos;  	
 	document.getElementById("utilidad-bruta").innerHTML = "utilidad bruta --------------------------------- " + utilidadBruta;  	
 }
