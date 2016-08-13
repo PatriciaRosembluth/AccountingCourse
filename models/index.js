44298d7dbe8193f6c70a76bd297a547998027de7
Patrica Rosembluth <patty_rosembluth@hotmail.com>
2016-08-12 22:32:13 -0400

    conceptos

--- models/index.js
+++ models/index.js
@@ -2,3 +2,7 @@ function initExcercise(){
 	localStorage.clear();
 	window.location.replace("http://localhost/AccountingCourse/planCuentas.html");
 }
+
+function conceptos(){
+	window.location.replace("http://localhost/AccountingCourse/conceptos.html");
+}
